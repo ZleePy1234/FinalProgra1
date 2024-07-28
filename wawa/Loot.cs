8627c18cf902dff7b383e678f4bd3bd5d32172fc
Alexander <zleepy2002@gmail.com>
2024-07-28 04:54:59 -0600

    Inicializacion de loot

--- wawa/Loot.cs
+++ wawa/Loot.cs
@@ -1,4 +1,12 @@
 namespace Loot
 {
-    
+    public class CreacionLoot
+    {
+        public struct LootData
+        {
+            public string lootName;
+            public int lootValue;
+            public int lootID;
+        }
+    }
 }
