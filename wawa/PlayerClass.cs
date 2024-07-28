e96bc9331a79240fc6b1dd311f1f1f615e200561
Alexander <zleepy2002@gmail.com>
2024-07-27 19:02:19 -0600

    Structs de personajes completados

--- wawa/PlayerClass.cs
+++ wawa/PlayerClass.cs
@@ -2,20 +2,26 @@ namespace PlayerClass
 {
     public class CreacionPlayer
     {
+        //struct para la creacion de ataques de las clases distintas
         public struct PlayerAttack
         {
             public string name;
             public int damage;
             public int block;
 
-            public PlayerAttack(string name, int damage, int block)
+            public bool isRanged;
+
+            public PlayerAttack(string name, int damage, int block, bool isRanged)
             {
                 this.name = name;
                 this.damage = damage;
                 this.block = block;
+                this.isRanged = isRanged;
             }
 
         }
+
+        //struct para la creacion de las estadisticas de cada clase asi como de sus ataques
         public struct PlayerData
         {
             public string playerName;
@@ -42,13 +48,30 @@ namespace PlayerClass
         //declaracion de clases
         //clase warlock
         #region Warlock
-        public static PlayerAttack Hacha = new PlayerAttack("Hacha de mano", 18,0);
+        public static PlayerAttack Hacha = new PlayerAttack("Hacha de mano", 18,0, false);
 
-        public static PlayerAttack EstallidoArcano = new PlayerAttack("Estallido arcano", 30, 0);
-        public static PlayerAttack  Bloquear = new PlayerAttack("Escudo Magico", 0, 20);
+        public static PlayerAttack EstallidoArcano = new PlayerAttack("Estallido arcano", 30, 0, true);
+        public static PlayerAttack  Bloquear = new PlayerAttack("Escudo Magico", 0, 25, false);
         public static PlayerData WarlockData = new PlayerData("Warlock",150, 0, Hacha, EstallidoArcano, Bloquear); 
 
         #endregion
+        //clase paladin
         #region Paladin
+
+        public static PlayerAttack Martillo = new PlayerAttack("Golpe de Martillo", 20, 0, false);
+        public static PlayerAttack Fuerza = new PlayerAttack("Emanar Fuerza", 12, 20, true);
+        public static PlayerAttack EscudoDivino = new PlayerAttack("Escudo Divino", 0, 50, false);
+        public static PlayerData PaladinData  = new PlayerData("Paladin",200, 0,Martillo, Fuerza, EscudoDivino);
+
+        #endregion
+
+        //clase mago
+
+        #region Mago
+
+        public static PlayerAttack Bastonazo = new PlayerAttack("Bastonazo", 10,0, false);
+        public static PlayerAttack BolaFuego = new PlayerAttack("Bola de Fuego", 35,0,true);
+        public static PlayerAttack BarreraMagica = new PlayerAttack("Barrera Magica", 0,15, false);
+        public static PlayerData MagoData = new PlayerData("Mago",125, 0,Bastonazo, BolaFuego, BarreraMagica);
     }
 }
