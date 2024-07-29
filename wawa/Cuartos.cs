b0778eeb2142cb0c0e3681f8868f2f94afbd9c66
Alexander <zleepy2002@gmail.com>
2024-07-29 17:49:10 -0600

    Agregada Lista de Strings con los nombres de los cuartos | Completado

--- wawa/Cuartos.cs
+++ wawa/Cuartos.cs
@@ -1,4 +1,24 @@
 namespace Cuartos
 {
-    
+    public class CreacionCuartos
+    {
+        public static List<string> CuartosList = new List<string>();
+
+        public static void ListaCuartos()
+        {
+            CuartosList.Add("una Cripta");
+            CuartosList.Add("una Caverna");
+            CuartosList.Add("un Altar");
+            CuartosList.Add("una Carcel");
+            CuartosList.Add("unas Barracas Abandonadas");
+            CuartosList.Add("un Pasillo");
+            CuartosList.Add("un Santuario Dilapidado");
+            CuartosList.Add("una Forja Apagada");
+            CuartosList.Add("las Fosas");
+            CuartosList.Add("una Armeria Vieja");
+            CuartosList.Add("una Alcoba Solitaria");
+            CuartosList.Add("una Libreria Quemada");
+            CuartosList.Add("un Comedor Dorado");
+        }
+    }
 }
